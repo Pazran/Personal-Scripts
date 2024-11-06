9e4f6c8b6e4048cbe2e47a5b006bc9b35f418c37
Pazran <lawrenerno@gmail.com>
2024-11-06 18:24:28 +0800

    initial

---
+++ find-duplicate-files.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/bash
+
+function cursorBack() {
+  echo -en "\033[$1D"
+  # Mac compatible, but goes back to first column always. See comments
+  #echo -en "\r"
+}
+
+IFS=$'\n'
+a=0
+FILE_TYPE="*.*"
+TEMP_FILE=$(mktemp '/tmp/dupe-list.XXXXXXXX')
+#mktemp $TEMP_FILE > /dev/null
+find Downloads -type f -name "$FILE_TYPE" -size -10M -exec md5sum {} + | sort -k 1,1 |  uniq -D -w 33 > $TEMP_FILE &
+
+pid=$!
+#spin='-\|/'
+#spin='⣾⣽⣻⢿⡿⣟⣯⣷'
+#spin='◐◓◑◒'
+#spin='┤┘┴└├┌┬┐'
+spin="▁▂▃▄▅▆▇█▇▆▅▄▃▂▁"
+#spin="▉▊▋▌▍▎▏▎▍▌▋▊▉"
+#spin='⠁⠂⠄⡀⢀⠠⠐⠈'
+z=0
+tput civis
+while ps -p $pid >/dev/null
+do
+    #z=$(((z+1)%4))
+    #printf "\rFinding duplicate files for file type: $FILE_TYPE... ${spin:$z:1}"
+    z=$(((z+3)%${#spin}))
+    printf "\rFinding duplicate files for file type: $FILE_TYPE... ${spin:$z:3}"
+    cursorBack 1
+    sleep .1
+done
+tput cnorm
+for i in $(cat $TEMP_FILE)
+do
+    printf "\n$i"
+    ((a++))
+done
+printf "%.0s\n" {1..2}
+printf '%s\n' "$a duplicate files found"
+rm $TEMP_FILE
